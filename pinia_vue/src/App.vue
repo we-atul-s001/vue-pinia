2fe5e9227f9967de69c913cbc8a0fac80ecf335a
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-06 10:43:28 +0530

    Pinia Installation

---
+++ pinia_vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{store.title}}</h1>
+
+    </div>
+</template>
+
+<script>
+import { useTaskStore } from './stores/taskStore';
+export default
+{
+    setup()
+    {
+        const store = useTaskStore();
+
+        return { store }
+    }
+}
+</script>
